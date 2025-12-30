a2756d844d0f3d639de575472d3b017b54dccdf8
Filipe Marçal <76597608+filipetmarcal@users.noreply.github.com>
2025-12-29 21:11:57 -0300

    Create card.py

---
+++ core/card.py
@@ -0,0 +1,27 @@
+class Card:
+    def __init__(
+        self,
+        nome,
+        tipo,
+        custo=0,
+        aparencia="",
+        descricao="",
+        pv=0,
+        dano=0
+    ):
+        self.nome = nome
+        self.tipo = tipo
+        self.custo = custo
+        self.aparencia = aparencia
+        self.descricao = descricao
+        self.pv = pv
+        self.dano = dano
+
+        self.inventario = []
+        self.partes = []
+
+    def esta_ativo(self):
+        return self.pv > 0
+
+    def receber_dano(self, valor):
+        self.pv -= valor
