17ceb2e2dcd27cd20db92d8ab3383211b8aab875
Filipe Marçal <76597608+filipetmarcal@users.noreply.github.com>
2026-01-02 20:02:51 -0300

    Update unit.py

--- core/unit.py
+++ core/unit.py
@@ -1,11 +1,22 @@
 from core.card import Card
 
 class Unit(Card):
-    def __init__(self, **kwargs):
+    def __init__(
+        self,
+        fome=0,
+        fadiga=0,
+        fome_max=100,
+        fadiga_max=100,
+        **kwargs
+    ):
         super().__init__(**kwargs)
+        self.fome = fome
+        self.fadiga = fadiga
+        self.fome_max = fome_max
+        self.fadiga_max = fadiga_max
+    def esta_faminto(self):
+        return self.fome >= self.fome_max
 
-    def adicionar_parte(self, parte):
-        self.partes.append(parte)
+    def esta_exausto(self):
+        return self.fadiga >= self.fadiga_max
 
-    def remover_parte(self, parte):
-        self.partes.remove(parte)
